3c0ac637ecefd289d29d519327c389fe0750e10b
mehdibkz82 <mehdibkz82@icloud.com>
2026-03-20 09:46:46 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -58,6 +58,7 @@ const AuthenticatedApp = () => {
           }
         />
       ))}
+      <Route path="/MissionHistory" element={<MissionHistory />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
