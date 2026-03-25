8d89b61dc714d3f4dbbfea7d56a5addca9279fd0
mehdibkz82 <mehdibkz82@icloud.com>
2026-03-25 11:16:29 +0000

    File changes

--- base44/functions/getProfessionals/entry.ts
+++ base44/functions/getProfessionals/entry.ts
@@ -3,11 +3,7 @@ import { createClientFromRequest } from 'npm:@base44/sdk@0.8.23';
 Deno.serve(async (req) => {
   try {
     const base44 = createClientFromRequest(req);
-
-    // Utilise le service role pour bypasser les RLS sur User
-    const allUsers = await base44.asServiceRole.entities.User.list('-created_date', 500);
-    const professionals = allUsers.filter(u => u.user_type === 'professionnel' && !u.account_deleted);
-
+    const professionals = await base44.asServiceRole.entities.Professional.list('-created_date', 200);
     return Response.json({ professionals });
   } catch (error) {
     console.error('getProfessionals error:', error);
