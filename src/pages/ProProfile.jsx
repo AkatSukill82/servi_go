7b1811a66f0c907444537f01da21af297f8a5d55
mehdibkz82 <mehdibkz82@icloud.com>
2026-03-26 06:10:37 +0000

    File changes

--- src/pages/ProProfile.jsx
+++ src/pages/ProProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
 import { base44 } from '@/api/base44Client';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { Input } from '@/components/ui/input';
