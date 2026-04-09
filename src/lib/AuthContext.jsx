b45bbbeda96809e33bcfa5ff51a3a83b8409e0df
mehdibkz82 <mehdibkz82@icloud.com>
2026-04-09 15:28:29 +0000

    File changes

--- src/lib/AuthContext.jsx
+++ src/lib/AuthContext.jsx
@@ -113,7 +113,8 @@ export const AuthProvider = ({ children }) => {
   const logout = () => {
     setUser(null);
     setIsAuthenticated(false);
-    base44.auth.logout('/Landing');
+    base44.auth.logout();
+    base44.auth.redirectToLogin();
   };
 
   const navigateToLogin = () => {
